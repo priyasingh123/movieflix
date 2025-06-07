13cc1f3adeb2a9c72a3cd4fe7e6dd19719cb423a
priya <priyaasinghh345@gmail.com>
2025-06-07 18:05:06 +0530

    change route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route
           exact
-          path="/movies"
+          path="/moviefix"
           element={
             <>
               <Navbar setMovies={setMovies} />
