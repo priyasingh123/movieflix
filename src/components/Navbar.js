fa0568bd0dbf4b4cb0e35be19a3740809f34c92d
priya <priyaasinghh345@gmail.com>
2025-06-07 07:19:05 +0530

    add genres

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,38 +5,53 @@ import { useState, useEffect } from 'react'
 
 const Navbar = ({setMovies}) => {
     const [filter, setFilter] = useState('all')
+    const [genres, setGenres] = useState([])
     const [page] = useState(1)
 
-    useEffect(() => {
-        const fetchMovies = async () => {
-            const baseUrl = process.env.REACT_APP_BASEURL
-            const apiKey = process.env.REACT_APP_APIKEY
-            const url = `${baseUrl}/discover/movie?api_key=${apiKey}&primary_release_year=2012&page=${page}&vote_count.gte=100&sort_by=popularity.desc`
+    const baseUrl = process.env.REACT_APP_BASEURL
+    const apiKey = process.env.REACT_APP_APIKEY
 
-            const res = await fetch(url)
-            const response = await res.json()
-            setMovies(response.results)
-        }
-        
+    const fetchMovies = async () => {
+        // const url = `${baseUrl}/discover/movie?api_key=${apiKey}&primary_release_year=2012&page=${page}&vote_count.gte=100&sort_by=popularity.desc&with_genres=${filter}`
+        const url = `${baseUrl}/discover/movie?api_key=${apiKey}&primary_release_year=2012&page=${page}&vote_count.gte=100&sort_by=popularity.desc`
+        const res = await fetch(url)
+        const response = await res.json()
+        setMovies(response.results)
+    }
+
+    const fetchGenres = async () => {
+        const url = `${baseUrl}/genre/movie/list?api_key=${apiKey}`
+        const res = await fetch(url)
+        const response = await res.json()
+        setGenres(response.genres)
+    }
+    useEffect(() => {
+        fetchGenres()
         fetchMovies()
     },[])
+    // },[filter])
 
     const handleFilter = (e) => {
         setFilter(e.target.id)
     }
 
-
+    let count = 0;
     return (
         <div className="navbar">
             <img className="navbar-img" src={img} alt="logo"/>
             <label className='logo-title'>MOVIEFIX</label>
             <div className='menu-items'>
                 <ul onClick={(e)=>handleFilter(e)}>
-                    <Link className={`list-item`} to={"/"} ><li id="all" className={` category ${filter === 'all'?'background-red':''}`}>All</li> </Link>
-                    <Link className={`list-item `} to={"/"} ><li id="action" className={` category ${filter === 'action'?'background-red':''}`}>Action</li> </Link>
-                    <Link className={`list-item`} to={"/"} ><li id="comedy" className={` category ${filter === 'comedy'?'background-red':''}`}>Comedy</li> </Link>
-                    <Link className={`list-item `} to={"/"} ><li id="horror" className={` category ${filter === 'horror'?'background-red':''}`}>Horror</li> </Link>
-                    <Link className={`list-item`} to={"/"} ><li id="drama" className={` category ${filter === 'drama'?'background-red':''}`}>Drama</li> </Link>
+                    {genres.map((genre) => {
+                        count++;
+                        if (count <= 10) {
+                            return (
+                                <Link className={`list-item`} to={"/"} >
+                                    <li key={genre.name} id={genre.name} className={` category ${filter === genre.name?'background-red':''}`}>{genre.name}</li> 
+                                </Link>
+                            )
+                        } return null;
+                    })}
                 </ul>
 
             </div>
