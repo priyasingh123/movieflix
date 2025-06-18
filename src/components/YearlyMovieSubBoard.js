1dbbde34879a6d313a54b668c76394b3d9944109
priya <priyaasinghh345@gmail.com>
2025-06-18 11:02:06 +0530

    add view more button

--- src/components/YearlyMovieSubBoard.js
+++ src/components/YearlyMovieSubBoard.js
@@ -1,8 +1,21 @@
 import MovieCard from "./MovieCard";
 
-const YearlyMovieSubBoard = ({ year, allMovies }) => {
+const YearlyMovieSubBoard = ({
+  year,
+  allMovies,
+  setPage,
+  page,
+  setSelectedYear,
+  setViewMoreMovies,
+}) => {
+  const viewAllMovies = () => {
+    setPage(page + 1);
+    setSelectedYear(year);
+    setViewMoreMovies(true);
+  };
+
   return (
-    <>
+    <section className="positioned-yearly-movie">
       <label className="year-label white-label">{year}</label>
       <div className="yearly-movie">
         {allMovies?.map((movie, index) => {
@@ -15,8 +28,11 @@ const YearlyMovieSubBoard = ({ year, allMovies }) => {
             />
           );
         })}
+        <button className="category view-more-btn" onClick={viewAllMovies}>
+          View More
+        </button>
       </div>
-    </>
+    </section>
   );
 };
 
